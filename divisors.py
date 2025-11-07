b8a2ec129cd3b09fe1c89f37f36c401ab3542953
rladpwns03-beep <rladpwns03@gachon.ac.kr>
2025-11-07 21:04:53 +0900

    HW uploaded_202233958 김예준
    Lab10 HW Answer

--- divisors.py
+++ divisors.py
@@ -1,1 +1,10 @@
+#divisor.py by Ye Jun Kim, 202233958, rladpwns03@gachon.ac.kr
 
+import sys
+number = int(sys.argv[1])
+
+for i in range(1,number+1):
+        if number%i == 0:
+                print(i, end=" ")
+
+print()
